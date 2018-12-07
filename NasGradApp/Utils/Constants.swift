be7de76b9f7ea7c44620ee2a18135e484ed1e7d3
Dorian Cizmar <d.cizmar@levi9.com>
2018-12-07 13:20:14 +0100

    Init commit

---
+++ NasGradApp/Utils/Constants.swift
@@ -0,0 +1,17 @@
+//
+//  Const.swift
+//  NasGradApp
+//
+//  Created by Dorian Cizmar on 12/7/18.
+//  Copyright © 2018 NasGrad. All rights reserved.
+//
+
+import Foundation
+
+struct Constants {
+    
+    struct Localizable {
+        static let test = "test"
+    }
+    
+}
